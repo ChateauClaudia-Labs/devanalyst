849b12e1f1322f5b740f258850007f7eb2b183e7
Hernandez <alejandro.hernandez@misys.com>
2019-02-21 18:41:08 -0800

    Kept adding tests; 7 so far

--- devanalyst/simulation/tests/umbrella_test.py
+++ devanalyst/simulation/tests/umbrella_test.py
@@ -5,18 +5,25 @@ import devanalyst.simulation.tests.test_timecard as tt
 
 
 class test_uss(TestCase):
-
     def test(self):
-        self.assertTrue(tt.test_uss_ACTUAL == tt.test_uss_EXPECTED)
+        self.assertTrue(tt.testOK('uss'))
 
-
-class test_fail(TestCase):
-
+class test_createTeams(TestCase):
     def test(self):
-        self.assertTrue(1 == 2)
+        self.assertTrue(tt.testOK('createTeams'))
 
-class test_pass(TestCase):
 
-    def test(self):
-        self.assertTrue(2 == 2)
+class test_userStoryCreate(TestCase):
+    def test_stories_df(self):
+        self.assertTrue(tt.testOK('test_userStoryCreate.stories_df'))
+    def test_estimates_df(self):
+        self.assertTrue(tt.testOK('test_userStoryCreate.estimates_df'))    
+    def test_workload_df(self):
+        self.assertTrue(tt.testOK('test_userStoryCreate.workload_df'))   
+    def test_crosscheck_df(self):
+        self.assertTrue(tt.testOK('test_userStoryCreate.crosscheck_df'))    
+    def test_avg_estimates_df(self):
+        self.assertTrue(tt.testOK('test_userStoryCreate.avg_estimates_df'))
+
+
 
