9b236f55528937b13742f817a8dca06d53307336
Alejandro Hernandez <alejandro.hernandez.chaves@gmail.com>
2019-03-07 12:39:10 -0800

    Recovery point for refactoring and inspection hooks

---
+++ devanalyst/simulation/tests/all_simm_test.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest import TestCase
+
+import devanalyst.simulation.tests.test_simm # Import needed to populate tu_.ACTUAL and tu_.EXPECTED
+
+import devanalyst.simulation.tests.test_utils as tu_
+
+# ----------------------------- Simulation Model tests ------------------------------
+
+class test_greedyAllocation(TestCase):
+    def test_Committed(self):
+        self.assertTrue(tu_.testOK('simm.test_greedyAllocation.Committed'))
+    def test_Tasks(self):
+        self.assertTrue(tu_.testOK('simm.test_greedyAllocation.Tasks')) 
+
+class test_balancedAllocation(TestCase):
+    def test_Committed(self):
+        self.assertTrue(tu_.testOK('simm.test_balancedAllocation.Committed'))
+    def test_Tasks(self):
+        self.assertTrue(tu_.testOK('simm.test_balancedAllocation.Tasks')) 
